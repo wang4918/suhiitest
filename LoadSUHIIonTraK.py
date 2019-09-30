ce6d5372478010fdef2079df35a0ca111f7c3335
Wang <Michael.Wang@dlr.de>
2019-09-30 13:41:00 +0200

    completed tex doc

--- LoadSUHIIonTraK.py
+++ LoadSUHIIonTraK.py
@@ -5,14 +5,13 @@ Created on Thu Sep 26 10:00:46 2019
 @author: wang_mc
 """
 
-
 import geopandas as gpd
 import pandas as pd
 import os
 from scipy import stats
 
 
-path = 'C:\\Users\\wang_mc\\Documents\\SUHII\\CSVs'
+path = 'C:\\Users\\wang_mc\\VSCode\\CSVs'
 os.chdir(path)
 
 def getSuhii(csvfile):
@@ -21,22 +20,22 @@ def getSuhii(csvfile):
 
 cityname = ['Addis_Abeba','Amsterdam','Ankara','Athens','Beijing','Berlin','Birmingham',
         'Bruxelles','Budapest','Casablanca','Chicago','Copenhagen','Dehli',
-        'Dubai','Geneva','Glasgow','Gothenburg','Hamburg','Helsinki','Hong_Kong',
-        'Izmir','Jerusalem','Johannesburg','Lisbon','London','Madrid','Medellin',
-        'Melbourne','Milan','Montreal','Munich','Nairobi','Niigata','Oslo',
-        'Paris','Pheonix','Portland','Prag','Roma','Santiago','Seoul','Shizouka',
-        'Singapore','Stockholm','Strasburg','Sydney','Taipeh','Tallin','Teheran',
+        'Dubai',"Dublin",'Geneva','Glasgow','Gothenburg','Hamburg','Helsinki',
+        'Hong_Kong','Izmir','Jerusalem','Johannesburg','Lisbon','London','Madrid',
+        'Medellin','Melbourne','Milan','Montreal','Munich','Nairobi','Niigata','Oslo',
+        'Paris','Phoenix','Portland','Prag','Roma','Santiago','Seoul','Shizouka',
+        'Singapore','Stockholm','Strasburg','Sydney','Taipeh','Tallinn','Teheran',
         'Tshwane','Turin','Vancouver','Vienna','Warsaw','Zuerich']
 
 for city in cityname:
     shp = gpd.read_file('C:\\Users\\wang_mc\\Documents\\SUHII\\UnloadedTrak\\%s_DIS_Indizes.shp'%city)
-    summer = pd.read_csv('%sSummer'%city)
-    winter = pd.read_csv('%sWinter'%city)
-    annual = pd.read_csv('%sAnnual'%city)
+    summer = pd.read_csv('%sSummer.csv'%city)
+    winter = pd.read_csv('%sWinter.csv'%city)
+    annual = pd.read_csv('%sAnnual.csv'%city)
     shp = shp.assign(SummerSUHII = getSuhii(summer))
     shp = shp.assign(WinterSUHII = getSuhii(winter))
     shp = shp.assign(AnnualSUHII = getSuhii(annual))
-    shp.to_file('%s.shp'%city)
+    shp.to_file('%s_DIS_Indizes.shp'%city)
 
 
 
--- tester.py
+++ tester.py
@@ -8,6 +8,25 @@ import os
 import math
 from scipy import stats
 
+def polyfit(x, y, degree):
+    results = {}
+
+    coeffs = np.polyfit(x, y, degree)
+
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    results['determination'] = ssreg / sstot
+
+    return results
+
 def plotisalst(summer, winter, annual, name, magorder):
   
   joined = summer.merge(annual, 'inner', 'isapercent', suffixes = ('summer', 'annual'))
@@ -24,14 +43,14 @@ def plotisalst(summer, winter, annual, name, magorder):
   for season in seasons:
     linreg = stats.linregress(joined['isapercent'], joined[season])
 
-    df = df.append({"Season": season,"SUHII": round(linreg[0]*100, 3), "R-Squared": round((linreg[2]*linreg[2]),3), "Std. Error": round(linreg[4],5)}, ignore_index=True)
+    df = df.append({"Season": season,"SUHII": round(linreg[0]*100, 3), "R-Squared": round(polyfit(joined['isapercent'], joined[season], magorder)['determination'],3), "Std. Error": round(linreg[4],5)}, ignore_index=True)
     
     
   g = sns.lmplot(x = 'isapercent', y = 'meanvalue', hue = 'season', truncate = True, data = melted,
               palette=dict(Summer='r',Annual='y',Winter='b', ), order = magorder, legend= False)
   g.set_axis_labels('ISA%', 'Mean LST (\u00b0C)')
   
-#  plt.ylim(0,45)
+  plt.ylim(0,45)
 
   plt.title('%s ISA vs Mean LST' %(name))
   plt.legend(loc='lower left', bbox_to_anchor=(-.15,-0.485),
@@ -46,19 +65,28 @@ def plotisalst(summer, winter, annual, name, magorder):
           cellLoc='right', rowLoc='center',
           loc='lower right', bbox=[0.15,-0.45,0.85,.28])
   plt.subplots_adjust(bottom=0.3)
-  plt.show()
+  plt.savefig('C:\\Users\\wang_mc\\VSCode\\Charts\\%s.png'%name)
   
 
-path = 'C:\\Users\\wang_mc\\Documents\\SUHII\\CSVs'
+path = 'C:\\Users\\wang_mc\\VSCode\\CSVs'
 os.chdir(path)
 
-summer = pd.read_csv('OsloSummer.csv')
-winter = pd.read_csv('OsloWinter.csv')
-annual = pd.read_csv('OsloAnnual.csv')
-plotisalst(summer, winter, annual, 'Oslo', 3)
+cityname = ['Addis_Abeba','Amsterdam','Ankara','Athens','Beijing','Berlin','Birmingham',
+        'Bruxelles','Budapest','Casablanca','Chicago','Copenhagen','Dehli',
+        'Dubai',"Dublin",'Geneva','Glasgow','Gothenburg','Hamburg','Helsinki',
+        'Hong_Kong','Izmir','Jerusalem','Johannesburg','Lisbon','London','Madrid',
+        'Medellin','Melbourne','Milan','Montreal','Munich','Nairobi','Niigata','Oslo',
+        'Paris','Phoenix','Portland','Prag','Roma','Santiago','Seoul','Shizouka',
+        'Singapore','Stockholm','Strasburg','Sydney','Taipeh','Tallinn','Teheran',
+        'Tshwane','Turin','Vancouver','Vienna','Warsaw','Zuerich']
 
-plt.figure()
-sns.residplot(summer['isapercent'], summer['mean'], order=3, color='r')
-sns.residplot(winter['isapercent'], winter['mean'], order=3, color='b')
-sns.residplot(annual['isapercent'], annual['mean'], order=3, color='y')
-plt.show()
+for city in cityname:
+  summer = pd.read_csv('%sSummer.csv'%city)
+  winter = pd.read_csv('%sWinter.csv'%city)
+  annual = pd.read_csv('%sAnnual.csv'%city)
+  plotisalst(summer, winter, annual, city, 1)
+  plt.figure()
+  sns.residplot(summer['isapercent'], summer['mean'], order=1, color='r')
+  sns.residplot(winter['isapercent'], winter['mean'], order=1, color='b')
+  sns.residplot(annual['isapercent'], annual['mean'], order=1, color='y')
+  plt.savefig('C:\\Users\\wang_mc\\VSCode\\Charts\\%sResid.png'%city)
